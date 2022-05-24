9a9b1e062a5a351dc81dc2312a038d5e221617c7
AshutoshJha98 <jashutosh0@gmail.com>
2022-05-24 12:51:20 +0530

    added nandhini review + dynamic review content change

--- thebrain.js
+++ thebrain.js
@@ -1,5 +1,6 @@
-var review_1_text = "He elevated the performance of the team and product with a Data-Driven Approach. He was a quick learner, open to listening and doing experiments.";
-var review_2_text = "He was the best PM ever.";
+const review_texts = ["He elevated the performance of the team and product with a Data-Driven Approach. He was a quick learner, open to listening and doing experiments.<br>&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;- Mayank Singh",
+                        "He was the best PM ever.<br><br><br>&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;- Nandhini Venkatesan"];
+const review_avatars = ["url('mayank_review_avatar.png')", "url('nandhini_review_avatar.png')"]
 var i = 0;
 
 function TextReviewCarousel()
@@ -7,12 +8,14 @@ function TextReviewCarousel()
     
     if(i==0)
     {
-        document.getElementById("review_text").innerHTML = review_1_text;
+        document.getElementById("review_text").innerHTML = review_texts[i];
+        document.getElementById("review_avatar").style.backgroundImage = review_avatars[i];
         i++;
     }
     else
     {
-        document.getElementById("review_text").innerHTML = review_2_text;
+        document.getElementById("review_text").innerHTML = review_texts[i];
+        document.getElementById("review_avatar").style.backgroundImage = review_avatars[i];
         i=0;
     }
     setTimeout("TextReviewCarousel();",5000);
