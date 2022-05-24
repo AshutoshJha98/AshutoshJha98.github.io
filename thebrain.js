6bc88c4f9714fa2cfb4d421dc0160a4d825104a8
AshutoshJha98 <jashutosh0@gmail.com>
2022-05-24 17:41:36 +0530

    added zubear review content

--- thebrain.js
+++ thebrain.js
@@ -1,12 +1,13 @@
-const review_texts = ["He elevated the performance of the team and product with a Data-Driven Approach. He was a quick learner, open to listening and doing experiments.<br>&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;- Mayank Singh",
-                        "He was the best PM ever.<br><br><br>&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;- Nandhini Venkatesan"];
-const review_avatars = ["url('mayank_review_avatar.png')", "url('nandhini_review_avatar.png')"]
+const review_texts = ["He elevated the performance of the team and product with a Data-Driven Approach. He was a quick learner, open to listening and doing experiments.<br><span style='bottom:-3vw;position:absolute;right:0vw;font-size:1.7vw;font-family:Roboto-LightItalic'>- Mayank Singh</span>",
+                        "He was the best PM ever.<br><span style='bottom:-8vw;position:absolute;right:0vw;font-size:1.7vw;font-family:Roboto-LightItalic'>- Nandhini Venkatesan</span>",
+                        "Bhain ne phod diya.<br><span style='bottom:-8vw;position:absolute;right:0vw;font-size:1.7vw;font-family:Roboto-LightItalic'>- Zubear Ansari</span>"];
+const review_avatars = ["url('mayank_review_avatar.png')", "url('nandhini_review_avatar.png')", "url('zubear_review_avatar.png')"]
 var i = 0;
 
 function TextReviewCarousel()
 {
     
-    if(i==0)
+    if(i<=1)
     {
         document.getElementById("review_text").innerHTML = review_texts[i];
         document.getElementById("review_avatar").style.backgroundImage = review_avatars[i];
